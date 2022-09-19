11559fa1f64a8375c5c1e2da96cafb61a5cfc029
Kendrick Xie <kendrickxie@gmail.com>
2022-09-19 18:48:12 -0500

    removed submodule and added UR_12idb files

---
+++ ur5_driver/ur5_driver/UR_12idb/test_robUR3.py
@@ -0,0 +1,25 @@
+import robUR3
+import time
+
+i = 1
+while True:
+    try:
+        rob = robUR3.UR3(name = '192.168.50.82', device = 2)
+        time.sleep(0.2)
+        print('Successful ur5 connection on attempt #{}'.format(i))
+        break
+    except:
+        print('Failed attempt #{}'.format(i))
+        i+=1
+        time.sleep(2)
+
+rob.camera.focus(450)
+
+# rob.bring_QR_to_camera_center(referenceName='QR')
+rob.camera.capture()
+rob.camera.decode()
+rob.relocate_camera()
+# rob.tilt_align()
+
+rob.terminate()
+
