c2d080a23258cab3dd3faae4b8c15856439fcc92
Kendrick Xie <kendrickxie@gmail.com>
2022-09-22 15:13:34 -0500

    fixed protective stop with new positions

--- ur5_driver/ur5_driver/ur5_driver.py
+++ ur5_driver/ur5_driver/ur5_driver.py
@@ -31,8 +31,7 @@ class UR5(UR_DASHBOARD):
         i = 1
         while True:
             try:
-                self.ur5 = UR3(name = self.IP, device = camera_index)     # Using UR3 with movel() results in protective stop
-                # self.ur5 = Robot(self.IP)
+                self.ur5 = UR3(name = self.IP, device = camera_index)
                 time.sleep(0.2)
                 print('Successful ur5 connection on attempt #{}'.format(i))
                 break
@@ -45,11 +44,7 @@ class UR5(UR_DASHBOARD):
         self.velocity = 0.2
 
         # Joint position of arm's home position
-        self.home = (0.8419385552406311, -2.293344636956686, 1.723344628010885, -0.9996248048594971, 4.718705177307129, 0.8619277477264404)
-
-        # CAMERA SETUP:
-        # print('Connecting to camera...')
-        # self.camera = camera(IP=IP, device = camera_index)
+        self.home = [-5.432596866284506, -1.7744094334044398, 2.659326140080587, -0.8687126797488709, 0.8495127558708191, -0.0038750807391565445]
 
 
         # GRIPPER SETUP:
@@ -80,7 +75,7 @@ class UR5(UR_DASHBOARD):
         '''Pick up from first goal position'''
 
         above_goal = deepcopy(pick_goal)
-        above_goal[2] += 0.20
+        above_goal[2] += 0.05
 
         print('Moving to home position')
         self.ur5.robot.movej(self.home, self.acceleration, self.velocity)
@@ -107,7 +102,7 @@ class UR5(UR_DASHBOARD):
         '''Place down at second goal position'''
 
         above_goal = deepcopy(place_goal)
-        above_goal[2] += 0.20
+        above_goal[2] += 0.05
 
         print('Moving to home position')
         self.ur5.robot.movej(self.home, self.acceleration, self.velocity)
@@ -142,16 +137,21 @@ class UR5(UR_DASHBOARD):
 
 if __name__ == "__main__":
 
-    pos1= [-0.22575, -0.65792, 0.39271, 2.216, 2.196, -0.043]
-    pos2= [0.22575, -0.65792, 0.39271, 2.216, 2.196, -0.043]
+    pos1= [ 0.19699137, -0.65753703, 0.17121313, 1.58650296, 0.00313809, 0.00971325]
+    pos2= [-0.20288636, -0.65752902, 0.17121757, 1.58646952, 0.00336391, 0.0097106 ]
     robot = UR5()
+
     # print(robot.ur5.getj())
-    # robot.transfer(pos1,pos2)
-    # robot.transfer(pos2,pos1)
-    # print(robot.camera.capture())
-    # robot.camera.save()
+    # print(robot.ur5.get_xyz())
+
+    robot.transfer(pos1,pos2)
+    robot.transfer(pos2,pos1)
+
+    # print(robot.ur5.camera.capture())
+    # robot.ur5.camera.save()
+    
     # robot.ur5.robot.movel(robot.home, robot.acceleration, robot.velocity)
-    robot.ur5.bring_QR_to_camera_center(referenceName='QR')
+    # robot.ur5.bring_QR_to_camera_center(referenceName='QR')
 
     robot.close()
     print('end')
